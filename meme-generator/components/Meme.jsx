c934a8c66995716da9ddd51a288adedd3c7e9cff
ekwunifeifenna <ifennaekwunife73@gmail.com>
2023-11-07 12:24:09 -0500

    Add image Add image to the project and make a function to render it to the screen Add some styling to it Add a meme data file to act like an api call to generate the top 100 most popular memes

--- meme-generator/components/Meme.jsx
+++ meme-generator/components/Meme.jsx
@@ -1,18 +1,53 @@
-import React from "react"
+import React, {useState} from "react"
+import memesData from "./memesData"
 
 function Meme(){
 
+    // const [memeImage, setMemeImage] = useState("http://i.imgflip.com/1bij.jpg")
+    const [meme, setMeme] = useState({
+        topText: "",
+        bottomText: "",
+        randomImage: "http://i.imgflip.com/1bij.jpg"
+    })
+
+    const [allMemeImages, setAllMemeImages] = useState(memesData)
+
+
+    // The function to get the random image on the page 
+    function getMemeImage(){
+        const memesArray = allMemeImages.data.memes
+        const rand = Math.floor(Math.random()*memesArray.length)
+        const url = memesArray[rand].url
+
+        setMeme(previousMeme => ({
+            ...previousMeme,
+            randomImage: url 
+
+        }))
+        
+        
+
+    }
+
+
+
     return(
         <main>
-            <form>
-                <div className="form-inputs">
-                <input type="text" />
+            <form className="meme--form">
 
-                <input type="text" />
+                <div className="form--inputs">
+
+                <input type="text" className="form--text" placeholder="Enter top text"/>
+ 
+                <input type="text"  className="form--text" placeholder="Enter bottom text"/>
                 </div>
 
-                <button>Generate new Image</button>
+                <button className="form--button" onClick={getMemeImage}>Generate new Image</button>
+
+                <img src={meme.randomImage} className="meme--image"/>
             </form>
+
+            
             
         </main>
     )
